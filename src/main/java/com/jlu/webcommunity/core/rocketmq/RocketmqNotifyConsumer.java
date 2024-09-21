71b82116f8eb7a3135e39cde52375d1752a53ed3
C-learner678 <1808903979@qq.com>
2024-09-22 02:43:31 +0800

    .

--- src/main/java/com/jlu/webcommunity/core/rocketmq/RocketmqNotifyConsumer.java
+++ src/main/java/com/jlu/webcommunity/core/rocketmq/RocketmqNotifyConsumer.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
